acc12a58ffc3d4182bcea5a6daaa9c73e4d5c87c
Narmin Elkilani <nelkilan@ptb-02009527.paris.inria.fr>
2025-07-09 11:41:04 +0200

    modify installation script

--- install_amilcar.sh
+++ install_amilcar.sh
@@ -14,3 +14,21 @@ source venv/bin/activate
 
 # Install the Python packages needed by the SmartMesh SDK inside the venv
 sudo venv/bin/pip install -r /home/pi/Amilcar/requirements.txt --default-timeout=100
+
+#cd /home/pi/Amilcar
+
+# Update package list and fix broken dependencies
+#sudo apt update
+#sudo apt install -y python3-venv python3-pip
+
+# Create the virtual environment 
+#python3 -m venv venv
+
+# Activate the virtual environment
+#source venv/bin/activate
+
+# Upgrade pip (inside venv)
+#pip install --upgrade pip
+
+# Install Python packages inside the venv
+#pip install -r requirements.txt --default-timeout=100
