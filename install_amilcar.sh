7c4205907e4b9f419a40edb409e227a5d8e8a8aa
nermine11 <narminlkilani@gmail.com>
2025-07-23 12:49:43 +0200

    modify installation script

--- install_amilcar.sh
+++ install_amilcar.sh
@@ -5,8 +5,8 @@
 ##############################################################
 
 # update
-apt update
-apt install -y python3-venv libjack-jackd2-dev jackd2 i2c-tools
+sudo apt update
+sudo apt install -y python3-venv libjack-jackd2-dev jackd2 i2c-tools
 
 
 # install and activate the virtual environment
@@ -14,6 +14,6 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Install the Python packages needed by Amilcar inside the venv
-venv/bin/pip install --upgrade pip
-venv/bin/pip install -r /home/pi/Amilcar/requirements.txt --default-timeout=100
+sudo venv/bin/pip install --upgrade pip
+sudo venv/bin/pip install -r /home/pi/Amilcar/requirements.txt --default-timeout=100
 
