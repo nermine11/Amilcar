d45df1eaf609842313d9850ed0f5a1e93579c5a8
nermine11 <narminlkilani@gmail.com>
2025-07-25 19:45:34 +0200

    remove temp saving feature

--- audio/record_audio.py
+++ audio/record_audio.py
@@ -199,9 +199,9 @@ def get_current_hour_filename():
     generates full output filename like ./recordings/20250715/hydrophone_20250715_13.wav
     """
     now      = time.gmtime()
-    folder_by_day   = os.path.join(base_dir, time.strftime("%Y-%m-%d", now))
+    folder_by_day   = os.path.join(base_dir, time.strftime("%Y%m%d", now))
     os.makedirs(folder_by_day, exist_ok=True)
-    filename = os.path.join(folder_by_day, time.strftime("hydrophone_%H-%M-%S.wav",now))
+    filename = os.path.join(folder_by_day, time.strftime("hydrophone_%H%M%S.wav",now))
     return filename, time.time()
 
 def save_audio_and_markers(buffer_data, markers, filename:str):
@@ -222,23 +222,13 @@ def save_audio_and_markers(buffer_data, markers, filename:str):
     cue_points = []
     for timestamp, sample_offset, gps_position in markers:
         ms    = int((timestamp%1) * 1000)
-        label = f"sample {sample_offset} at {time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timestamp))}.{ms:04d}"
+        label = f"sample {sample_offset} at {time.strftime('%Y-%m-%d %H%M%S', time.gmtime(timestamp))}.{ms}"
         if gps_position:
             label += f" (position: {gps_position[0]}, {gps_position[1]})"
         else:
             label += "_,_"
         cue_points.append((sample_offset, label))
     write_cue_markers(filename, cue_points)
-
-def periodic_temp_saver(temp_filename):
-    """
-    Every 10 seconds, save a temporary version of the audio and markers.
-    Prevents full data loss on power loss.
-    """
-    while not event.is_set():
-        with buffer_lock:
-            save_audio_and_markers(active_frames, active_markers, temp_filename)
-        time.sleep(10)
     
 # ========== Start Recording ==========
 
@@ -266,9 +256,6 @@ with client:
             print(f" Failed to connect {port_name}: {e}")
     #intial filename and hour
     output_filename, current_hour = get_current_hour_filename()
-    base, extention = os.path.splitext(output_filename)
-    temp_filename = base + "_tmp" + extention
-    threading.Thread(target=periodic_temp_saver, args=(temp_filename,), daemon=True).start()
     try:
         #checks if a shutdown flag has been raised(CTRL c or power cut)
         while not event.is_set():
@@ -286,15 +273,10 @@ with client:
                 save_thread.start()
                 #update filename and hour
                 output_filename, current_hour = get_current_hour_filename()
-                base, extention = os.path.splitext(output_filename)
-                temp_filename = base + "_tmp" + extention
                 time.sleep(0.1)
     except KeyboardInterrupt:
         event.set() # shutdown will be called
-
+    
 #if CTRL_C has been pressed in the middle of recording
 with buffer_lock:
     save_audio_and_markers(active_frames, active_markers, output_filename)
-    # If a temp file exists, promote it to final filename
-    if os.path.exists(temp_filename):
-        os.replace(temp_filename, output_filename)
