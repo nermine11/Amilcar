44b49119be14a0755fcc3dc2fca32352b1191830
nermine11 <narminlkilani@gmail.com>
2025-07-26 16:27:47 +0200

    remove disablin networkManger

--- audio/reduce_latency_on_boot.sh
+++ audio/reduce_latency_on_boot.sh
@@ -27,8 +27,8 @@ EOF
 #sudo apt purge --auto-remove xdg*  
 sudo apt purge --auto-remove pulseaudio  
 sudo apt purge --auto-remove triggerhappy 
-sudo systemctl stop NetworkManager
-sudo systemctl disable NetworkManager
+#sudo systemctl stop NetworkManager
+#sudo systemctl disable NetworkManager
 
 
 SERVICES=(
